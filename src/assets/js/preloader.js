e06aaa2511f24cd5f294bf542f9a7d42e61f7690
Carlos Enrique Oropeza <ceon20020508@gmail.com>
2024-02-15 21:33:06 -0500

    videoJob, removing clgs, new clauses for gitignore

--- src/assets/js/preloader.js
+++ src/assets/js/preloader.js
@@ -2,7 +2,6 @@ import preloaderStyles from "../../css/preloaderStyles.module.css";
 
 export const preloader = () => {
   const body = document.getElementById("body");
-  console.log(body);
   if (body) {
     body.classList.add(preloaderStyles.putBackground);
     setTimeout(() => {
