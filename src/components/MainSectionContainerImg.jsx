e33eae53fd6759811285ef06d1109ffee0ab6418
Carlos Enrique Oropeza <ceon20020508@gmail.com>
2024-03-26 21:27:48 -0500

    Removing lazy loading for main photo

--- src/components/MainSectionContainerImg.jsx
+++ src/components/MainSectionContainerImg.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import ceonImg from "../imgs/Ceon_Img.webp";
 import "../scss/mainSectionStyles.scss";
 
 const MainSectionContainerImg = () => {
-  const [loaded, setLoaded] = useState(false);
-
   return (
-    <div className={`profilePhoto  ${loaded ? "show-img" : "hide-img"} `}>
+    <div className={`profilePhoto`}>
       <img
         src={`${ceonImg}`}
         alt="Carlos Oropeza foto"
         title="Carlos Oropeza foto"
-        onLoad={() => setLoaded(true)}
       />
     </div>
   );
